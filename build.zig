100246e4c2cbc140bc1013327c020c51a5cc4df0
Syed <syeds.devops007@gmail.com>
2024-07-29 00:40:32 +0530

    remove pretty

--- build.zig
+++ build.zig
@@ -24,9 +24,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    exe.linkSystemLibrary("c");
-    const pretty = b.dependency("pretty", .{ .target = target, .optimize = optimize });
-    exe.root_module.addImport("pretty", pretty.module("pretty"));
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
